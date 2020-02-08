d0d1802e46accd06e04e7aa83b9fd42533358fe2
Rick Kiessig <rick@12titans.net>
2020-02-08 20:58:12 +1300

    Healthy house pages and a few supplements. Moved to DWT template.

---
+++ Scripts/_vti_cnf/jquery.easing.1.3.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Jul 2016 05:37:41 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|appointments.html contact.html carnitine.html about.html floor.html master.dwt pricing.html floorplan.html healthy-house-intro.html protocol.html foundation.html siding.html calcium-d-glucarate.html electrical.html index.html framing.html location.html plumbing.html roofing.html windows.html resources.html kitchen.html alpha-lipoic-acid.html vitamin-e.html
+vti_author:SR|BLACKBOX\\Rick
+vti_modifiedby:SR|BLACKBOX\\Rick
+vti_timecreated:TR|29 Jul 2016 05:37:41 -0000
+vti_cacheddtm:TX|29 Jul 2016 05:37:41 -0000
+vti_filesize:IR|8097
---
+++ Scripts/_vti_cnf/site.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|07 Feb 2020 01:13:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|BLACKBOX\\Rick
+vti_modifiedby:SR|BLACKBOX\\Rick
+vti_timecreated:TR|07 Feb 2020 01:13:58 -0000
+vti_cacheddtm:TX|07 Feb 2020 01:13:58 -0000
+vti_filesize:IR|0
+vti_backlinkinfo:VX|
